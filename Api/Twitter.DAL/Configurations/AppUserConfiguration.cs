936246d433a90ad84051aaed8eb1476af29c77a0
moriongit <tempro.gmai@gmail.com>
2023-12-30 12:50:59 +0400

    Salam

---
+++ Api/Twitter.DAL/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twitter.Core.Entities;
+
+namespace Twitter.DAL.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<Topic> builder)
+        {
+            builder.Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(32);
+        }
+
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Api/Twitter.DAL/Contexts/TwitterContext.cs
+++ Api/Twitter.DAL/Contexts/TwitterContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Reflection;
 using Twitter.Core.Entities;
@@ -7,12 +8,13 @@ using Twitter.DAL.Configurations;
 
 namespace Twitter.DAL.Contexts
 {
-    public class TwitterContext : DbContext
+    public class TwitterContext : IdentityDbContext
     {
         public TwitterContext(DbContextOptions opt) : base(opt)
         {
         }
         public DbSet<Topic> Topics { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             var entries = ChangeTracker.Entries<BaseEntity>();
